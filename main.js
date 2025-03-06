7d4d0d5ba487916e4587dc660427dd3c3f7c0655
alaa elmahy <3laa3bdel3ziz@gmail.com>
2025-03-06 05:31:15 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let img = document.querySelector('.mainImg');
+let container = document.querySelector('body');
+function phones(phone){
+    img.src = phone;
+}
+function colors(color){
+    container.style.backgroundColor = color;
+}
